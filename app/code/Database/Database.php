e83ba15cd2141aebb5084853c61e7de63e26dc5e
Rajnish Kumar <rajnishmsrit@gmail.com>
2017-06-28 14:03:22 +0530

    Basic setup

---
+++ app/code/Database/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Code\Database;
+
+Class Database
+{
+
+  public function run()
+  {
+    return $this;
+  }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use App\Code\Database\Database;
+
+$data = new Database();
+
+echo "Hello World" ;
+
+?>
